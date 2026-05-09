0068e9b7a85685a31fe798912230d0052d2374b2
AADESHak007 <123197632+AADESHak007@users.noreply.github.com>
2026-05-09 21:25:51 +0530

    FEAT: audit data persistance and queue impelementation with testing done

--- app/api/v1/audit/route.ts
+++ app/api/v1/audit/route.ts
@@ -1,19 +1,51 @@
+import { prisma } from "@/lib/prisma";
+import { auditQueue } from "@/lib/queue";
 import { NextResponse } from "next/server";
 
 
-export  async function POST(req:Request , res:Response){
+export  async function POST(req:Request){
+try {
+        
+        //get the reqd response from body
+        const {tool , teamSize , plan ,billing ,tasks ,email,company} = await req.json() ;
+        
+        //saving the inital data in the DB and getting the ID ... 
+        const audit =  await prisma.audit.create({
+            data: {
+                input_tool:tool,
+                input_team_size:teamSize,
+                input_plan:plan,
+                input_billing:billing,
+                input_tasks:tasks,
+                input_email:email,
+                input_company:company,
+                status:"pending"
+            }
+        })
     
-    //get the reqd response from body
-    const {tool , teamSize , plan ,billing ,tasks ,email,company} = await req.json() ;
-    //return the data in response and the job-id for now : 
-    
+        //send the data to the queue ... 
+        await auditQueue.add("process-audit" ,{
+            auditId: audit.id
+        }) ;
+        
+        //return the job-id ...
+        
+        return NextResponse.json({
+            message : "audit - created",
+            success : true,
+            jobId : audit?.id, 
+            status:audit?.status
+        },{
+            status : 201
+        })
+} catch (error) {
     return NextResponse.json({
-        message : "audit - created",
-        success : true,
-        data : {tool , teamSize , plan ,billing ,tasks ,email,company},
-        jobId : "123" // static for now 
+        message : "internal server error",
+        success : false,
+        error : error || "unknown error"
     },{
-        status : 201
+        status : 500
     })
+}
 
 }
